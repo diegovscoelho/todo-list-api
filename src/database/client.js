2229d49c0d1cee5c38d55eddf8e52377b63d7ae4
diegovscoelho <dcoelhocarneiro@gmail.com>
2025-11-04 13:42:40 -0300

    Configurando pra rodar no Render

--- src/database/client.js
+++ src/database/client.js
@@ -1,13 +1,10 @@
-import pkg from 'pg';
+import { Pool } from 'pg';
 
-const { Pool } = pkg;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST, 
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+    connectionString: connectionString, 
+    ssl: connectionString ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
